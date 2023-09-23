62020945b44611970ae21f79d89249d804492f58
Michael RANIVO <michael.ranivo@epitech.eu>
2023-09-23 18:27:21 +0200

    Test workflow init backend TF 0.9

--- terraform/back/containers.tf
+++ terraform/back/containers.tf
@@ -18,6 +18,7 @@ data "azurerm_resource_group" "resource_group_TCLO_901" {
 
 output "id" {
   value = data.azurerm_resource_group.resource_group_TCLO_901.id
+  
 }
 
 #Je prends les datas du resources groupe puis les datas du compte de stockage

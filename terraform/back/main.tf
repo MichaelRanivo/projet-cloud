e462a51fa0c9002dadf9facb267a0bd4d16bfb7d
Michael RANIVO <michael.ranivo@epitech.eu>
2023-09-23 20:49:57 +0200

    test backend pipeline 0.4

--- terraform/back/main.tf
+++ terraform/back/main.tf
@@ -1,10 +1,14 @@
 terraform {
+
   required_providers {
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
+    
   }
+
   backend "azurerm" {
     resource_group_name  = "t-clo-907-lyo-3"
     storage_account_name = "atclo901lyo323092023"

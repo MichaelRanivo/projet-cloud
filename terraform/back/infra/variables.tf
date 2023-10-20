f0c4b56cafb5120fcc1483bbacb60cba3ba0f6d7
Michael Ranivo <michael.ranivo@epitech.eu>
2023-10-20 09:40:28 +0200

    test backend infra 1.3

--- terraform/back/infra/variables.tf
+++ terraform/back/infra/variables.tf
@@ -12,7 +12,7 @@ variable "backend_storage_account_name" {
     type        = string
     description = "The name of the storage account for the backend Terraform"  
 }
-
+ 
 variable "storage_account_tier" {
     type        = string
     description = "The storage account tier"

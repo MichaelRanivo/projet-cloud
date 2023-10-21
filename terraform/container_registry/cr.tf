fa295cea343ead5a359d93a2207b74b85118a474
Michael Ranivo <michael.ranivo@epitech.eu>
2023-10-21 23:55:13 +0200

    tf cr 0.3

--- terraform/container_registry/cr.tf
+++ terraform/container_registry/cr.tf
@@ -1,5 +1,14 @@
+# Generate random value for the name
+resource "random_string" "name" {
+  length  = 5
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
 resource "azurerm_container_registry" "cr_sample_app" {
-  name                = var.container_registry_name
+  name                = lower(join("", [var.container_registry_name,random_string.name.result]))
   resource_group_name = azurerm_resource_group.cr_resource_group.name
   location            = azurerm_resource_group.cr_resource_group.location
   sku                 = "Standard"

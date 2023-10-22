cfdfcad88ea3fa14355fa3d0b175f45ea2aa8ac9
Michael Ranivo <michael.ranivo@epitech.eu>
2023-10-22 15:15:52 +0200

    tf cr 1.2

--- terraform/container_registry/outputs.tf
+++ terraform/container_registry/outputs.tf
@@ -6,3 +6,7 @@ output "admin_password" {
   sensitive = true
   value     = azurerm_container_registry.cr_sample_app.admin_password
 }
+
+output "server_cr" {
+  value = azurerm_container_registry.cr_sample_app.login_server
+}

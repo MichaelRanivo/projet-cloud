c4188c9f2a04ba3d15c1ca1e056b2fe9357c5fd9
Michael RANIVO <michael.ranivo@epitech.eu>
2023-11-05 00:10:42 +0100

    rework some TF and Workflows Files

--- terraform/ImageVM/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.image_vm.public_ip_address
-}
-
-output "key_data" {
-  value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).privateKey
-}

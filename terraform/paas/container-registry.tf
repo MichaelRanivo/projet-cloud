1aada14e7210aa5455769d3557e0601404cd8684
Michael RANIVO <michael.ranivo@epitech.eu>
2023-12-04 20:35:15 +0100

    test aks deployment

--- terraform/paas/container-registry.tf
+++ terraform/paas/container-registry.tf
@@ -1,4 +1,4 @@
 data "azurerm_container_registry" "rc-data" {
     name                = join("",[var.container_registry_name,"zrmod"])
-    resource_group_name = var.resource_group_container_registry
+    resource_group_name = var.resource_group_container_registry 
 }

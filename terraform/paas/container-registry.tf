73944c1825f7e82a8eae8300081fbdd529f824e3
Michael RANIVO <michael.ranivo@epitech.eu>
2023-12-14 20:03:48 +0100

    modif some variable

--- terraform/paas/container-registry.tf
+++ terraform/paas/container-registry.tf
@@ -1,4 +1,4 @@
 data "azurerm_container_registry" "rc-data" {
-    name                = join("",[var.container_registry_name,"zrmod"])
+    name                = join("",[var.container_registry_name,"teakr"])
     resource_group_name = var.resource_group_container_registry 
 }

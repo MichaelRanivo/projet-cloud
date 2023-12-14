7463bcb094fc1804b73556bcb146e6544ea9cbb8
Michael Ranivo <michael.ranivo@epitech.eu>
2023-12-14 15:00:11 +0100

    re run the deploy again

--- terraform/back/infra/resource_group.tf
+++ terraform/back/infra/resource_group.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "tfstate_storage_resource_group" {
   name      = var.backend_resource_group_name
   location  = var.azure_cloud_location
   tags      = var.tags
-}  
+}
 
 resource "azurerm_storage_account" "tfstate_storage_account" {
   name                            = join("",[var.backend_storage_account_name,random_string.resource_code.result])
@@ -31,7 +31,7 @@ resource "azurerm_storage_account" "tfstate_storage_account" {
   account_replication_type        = var.storage_account_replication_type
   allow_nested_items_to_be_public = false
   tags                            = var.tags
-}  
+}
 
 resource "azurerm_storage_container" "tfstate_storage_container" {
   name                  = var.storage_container_name

f8aa98e35b4580a6e719f60616de933565091818
Michael Ranivo <michael.ranivo@epitech.eu>
2023-12-17 23:12:41 +0100

    finish tf iaas workflow and first test ansible

--- terraform/iaas/outputs.tf
+++ terraform/iaas/outputs.tf
@@ -7,29 +7,29 @@ resource "local_file" "node_master_private_key" {
   filename = "node_master_private_key.pem"
 }
 
-output "node_worker_1_pribate_ip_address" {
+output "node_worker_0_pribate_ip_address" {
   value = azurerm_linux_virtual_machine.node_worker_vm[0].private_ip_address
 }
 
-output "node_worker_2_pribate_ip_address" {
+output "node_worker_1_pribate_ip_address" {
   value = azurerm_linux_virtual_machine.node_worker_vm[1].private_ip_address
 }
 
-output "node_worker_3_pribate_ip_address" {
+output "node_worker_2_pribate_ip_address" {
   value = azurerm_linux_virtual_machine.node_worker_vm[2].private_ip_address
 }
 
-resource "local_file" "node_worker_1_private_key" {
+resource "local_file" "node_worker_0_private_key" {
   content  = jsondecode(azapi_resource_action.node_worker_ssh_public_key_gen[0].output).privateKey
+  filename = "node_worker_0_private_key.pem"
+}
+
+resource "local_file" "node_worker_1_private_key" {
+  content  = jsondecode(azapi_resource_action.node_worker_ssh_public_key_gen[1].output).privateKey
   filename = "node_worker_1_private_key.pem"
 }
 
 resource "local_file" "node_worker_2_private_key" {
-  content  = jsondecode(azapi_resource_action.node_worker_ssh_public_key_gen[1].output).privateKey
-  filename = "node_worker_2_private_key.pem"
-}
-
-resource "local_file" "node_worker_3_private_key" {
   content  = jsondecode(azapi_resource_action.node_worker_ssh_public_key_gen[2].output).privateKey
-  filename = "node_worker_3_private_key.pem"
+  filename = "node_worker_2_private_key.pem"
 }

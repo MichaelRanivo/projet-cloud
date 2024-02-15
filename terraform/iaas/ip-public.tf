6f1b6618b1a27c6c81955fad14f7147a69bcb39a
Michael Ranivo <michael.ranivo@epitech.eu>
2024-02-15 16:39:38 +0100

    test deploy iaas infra

--- terraform/iaas/ip-public.tf
+++ terraform/iaas/ip-public.tf
@@ -1,4 +1,4 @@
 data "azurerm_public_ip" "lb-adress-ip" {
     name                = var.iaas_address_ip_name
-    resource_group_name =  var.rg_iaas_address_ip_name
+    resource_group_name =  var.rg_iaas_address_ip_name 
 }
